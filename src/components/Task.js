41bb58f7ede3141658e649685747d13f7e623509
Dmitry Zhukov <ZhukovD.98@ya.ru>
2020-10-27 19:31:56 +0300

    Task toggle fixe

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,22 +4,27 @@ import { deleteTask, editTask, toggleTask } from '../actions'
 
 // import "./MyComponent.css"
 
-const Task = ({ id, text, done, isEditing, dispatch }) => {
-  let delBtn, editBtn, input;
+const Task = ({ id, text, done, dispatch }) => {
+  let input;
+  const toggleEdit = () => {
+  	if(input.disabled) input.removeAttribute("disabled");
+  				  else input.disabled="true";
+  }
+  const toggleDone = (e) => {
+  	if(e.target.tagName !== "A" && input.disabled) dispatch(toggleTask(id, input.value)) 
+  }
   return (
-    <li 
-    	className="task" 
-    	onClick={(e)=>{ dispatch(toggleTask(id, input.value)) }}	
-    >
+    <li className="task" onClick={toggleDone}>
     	<input 
     		ref={node => (input = node)} 
     		value={text}
     		onChange={(e)=>{ dispatch(editTask(id, input.value)) }}
-		    style={{
+	    	style={{
 		      textDecoration: done ? 'line-through' : 'none'
 		    }}
+		    disabled
 		/>
-    	<a className="task-edit-btn">ED</a>
+    	<a className="task-edit-btn" onClick={toggleEdit}>ED</a>
     	<a className="task-del-btn" onClick={(e)=>{ dispatch(deleteTask(id)) }}>DEL</a>
     </li>
   );
